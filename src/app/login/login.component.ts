6741e46caec9a77afc336ccba6203fe526b7daa7
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2021-01-20 01:45:13 -0300

    Melhorias no formulário de registro de Usuário

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -42,10 +42,10 @@ export class LoginComponent {
         .salvar(usuario)
         .subscribe( response => {
             this.mensagemSucesso = "Cadastro realizado com sucesso! Efetue o login.";
-            //this.cadastrando = false;
-            //this.username = '';
-            //this.password = '';
-            //this.errors = []
+            this.cadastrando = false;
+            this.username = '';
+            this.password = '';
+            this.errors = []
         }, errorResponse => {
             this.mensagemSucesso = null;
             this.errors = errorResponse.error.errors;
